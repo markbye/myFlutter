e97ef16841fc8ed78e996db4a35bbe005117eb5b
markbye <markbye@163.com>
2018-09-03 22:34:11 +0800

    add simple dialog

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,66 @@ import 'package:http/http.dart' as http;
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  final Widget child;
+
+  /// The distance that the child's left edge is inset from the left of the stack.
+  ///
+  /// Only two out of the three horizontal values ([left], [right], [width]) can be
+  /// set. The third must be null.
+  ///
+  /// If all three are null, the [Stack.alignment] is used to position the child
+  /// horizontally.
+  final double left;
+
+  /// The distance that the child's top edge is inset from the top of the stack.
+  ///
+  /// Only two out of the three vertical values ([top], [bottom], [height]) can be
+  /// set. The third must be null.
+  ///
+  /// If all three are null, the [Stack.alignment] is used to position the child
+  /// vertically.
+  final double top;
+
+  /// The distance that the child's right edge is inset from the right of the stack.
+  ///
+  /// Only two out of the three horizontal values ([left], [right], [width]) can be
+  /// set. The third must be null.
+  ///
+  /// If all three are null, the [Stack.alignment] is used to position the child
+  /// horizontally.
+  final double right;
+
+  /// The distance that the child's bottom edge is inset from the bottom of the stack.
+  ///
+  /// Only two out of the three vertical values ([top], [bottom], [height]) can be
+  /// set. The third must be null.
+  ///
+  /// If all three are null, the [Stack.alignment] is used to position the child
+  /// vertically.
+  final double bottom;
+
+  /// The child's width.
+  ///
+  /// Only two out of the three horizontal values ([left], [right], [width]) can be
+  /// set. The third must be null.
+  ///
+  /// If all three are null, the [Stack.alignment] is used to position the child
+  /// horizontally.
+  final double width;
+
+  /// The child's height.
+  ///
+  /// Only two out of the three vertical values ([top], [bottom], [height]) can be
+  /// set. The third must be null.
+  ///
+  /// If all three are null, the [Stack.alignment] is used to position the child
+  /// vertically.
+  final double height;
+
+  final Function onTapOutside;
+
+  const MyApp({Key key, this.left, this.top, this.right, this.bottom, this.width, this.height, this.child, this.onTapOutside})
+      : super(key: key);
   @override
 //  Widget build(BuildContext context) {
 //    final wordPair = new WordPair.random();
@@ -206,6 +266,29 @@ class RandomWordsState extends State<RandomWords> {
             onTap: () {
               print('BUtton was tapped111');
               getData();
+              showDialog<Null>(
+                  context: context,
+//                  child: new SimpleDialog();
+
+                  child: new SimpleDialog(
+                      title: new Text('ttttt'),
+                      titlePadding: const EdgeInsets.fromLTRB(50.0, 0.0, 0.0, 0.0),
+                      contentPadding:const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+                      children: [new Text('children'),
+                      _buildImg('bg2.jpg'),
+                      new Semantics(namesRoute: true, child: new Text('xxxx')),
+                      ]
+//                      content: new Text('okok'),
+//                      actions: <Widget>[
+//                        new FlatButton(
+//                            onPressed: () {
+//                              Navigator.pop(context);
+//                            },
+//                            child: new Text('确定')
+//                        )
+//                      ]
+                  )
+              );
             },
             child: new Container(
               //Container：矩形控件，可与BoxDecoration配合来装饰 background, a border, or a shadow，
